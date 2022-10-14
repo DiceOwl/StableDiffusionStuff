50287698bd2c2054c311922ba492cfee760636b5
DiceOwl <114508428+DiceOwl@users.noreply.github.com>
2022-10-14 20:55:41 +0200

    initial upload

---
+++ interpolate.py
@@ -0,0 +1,311 @@
+import numpy as np
+from tqdm import trange
+
+from copy import copy
+
+import modules.scripts as scripts
+import gradio as gr
+
+import random
+from PIL import Image, ImageFilter, ImageOps
+
+from modules import processing, shared, sd_samplers, images, masking
+from modules.processing import Processed
+from modules.sd_samplers import samplers
+from modules.shared import opts, cmd_opts, state
+import modules.images as images
+
+import re
+
+
+#re_int = re.compile(r'([+-]?[0-9]*[.]?[0-9]+)[~]([+-]?[0-9]*[.]?[0-9]+)', flags=re.MULTILINE)
+re_int = re.compile(r'([+-]?[0-9]*[.]?[0-9]+)([@][0-9]*[.][0-9]+)?[~]([+-]?[0-9]*[.]?[0-9]+)([@][0-9]*[.][0-9]+)?', flags=re.MULTILINE)
+
+re_range_float = re.compile(r"\s*([+-]?\s*\d+(?:.\d*)?)\s*-\s*([+-]?\s*\d+(?:.\d*)?)(?:\s*\(([+-]\d+(?:.\d*)?)\s*\))?\s*")
+
+re_range_count = re.compile(r"\s*([+-]?\s*\d+)\s*-\s*([+-]?\s*\d+)(?:\s*\[(\d+)\s*\])?\s*")
+re_range_count_float = re.compile(r"\s*([+-]?\s*\d+(?:.\d*)?)\s*-\s*([+-]?\s*\d+(?:.\d*)?)(?:\s*\[(\d+(?:.\d*)?)\s*\])?\s*")
+
+def apply_prompt_interpolate(p, x):
+    def f_interpolate(matchobj):
+        y1 = float(matchobj[1])
+        #y2 = float(matchobj[2])
+        if matchobj[2]:
+            a = float(matchobj[2][1:])
+        else:
+            a = 0
+        y2 = float(matchobj[3])
+        if matchobj[4]:
+            b = float(matchobj[4][1:])
+        else:
+            b = 1
+        if x<= a:
+            i = y1
+        elif x>= b:
+            i = y2
+        else:
+            i = y1 + (x-a)*(y2-y1)/(b-a)
+        return f'{i:.3f}'
+    p.prompt = re_int.sub(f_interpolate, p.prompt)
+    p.negative_prompt = re_int.sub(f_interpolate, p.negative_prompt)
+    pass
+
+
+def process_int(vals):
+    valslist = [x.strip() for x in vals.split(",")]
+
+    valslist_ext = []
+
+    for val in valslist:
+        m = re_range_float.fullmatch(val)
+        mc = re_range_count_float.fullmatch(val)
+        if m is not None:
+            start = float(m.group(1))
+            end = float(m.group(2))
+            step = float(m.group(3)) if m.group(3) is not None else 1
+
+            valslist_ext += np.arange(start, end + step, step).tolist()
+        elif mc is not None:
+            start = float(mc.group(1))
+            end   = float(mc.group(2))
+            num   = int(mc.group(3)) if mc.group(3) is not None else 1
+            
+            valslist_ext += np.linspace(start=start, stop=end, num=num).tolist()
+        else:
+            valslist_ext.append(val)
+
+        valslist = valslist_ext
+
+    valslist = [float(x) for x in valslist]
+
+    return valslist
+
+
+class Script(scripts.Script):
+    def title(self):
+        return "Interpolate"
+
+    def show(self, is_img2img):
+        return is_img2img
+
+    def ui(self, is_img2img):
+        init_img2 = gr.Image(label="alternate img2img imgage", elem_id="img2img_image_alternate", show_label=False, source="upload", interactive=True, type="pil")     
+        
+        i_values = gr.Textbox(label="interpolation values", lines=1)
+        
+        def gr_show(visible=True):
+            return {"visible": visible, "__type__": "update"}
+
+        def change_visibility(show):
+            return {comp: gr_show(show) for comp in loopback_vis}
+            
+        loopback_vis = []
+        
+        loopback_toggle = gr.Checkbox(label='Loopback', value=False)
+        
+        with gr.Box(visible=False) as loopback_box:           
+            loopback_vis.append(loopback_box)
+            loopback_loops = gr.Slider(minimum=1, maximum=32, step=1, label='Refinement loops', value=1)
+            loopback_alpha = gr.Slider(minimum=0.0, maximum=1.0, step=0.005, label='Loopback alpha', value=0.2)
+            border_alpha = gr.Slider(minimum=0.0, maximum=1.0, step=0.005, label='Border alpha', value=0.1)
+            blend_strides = gr.Slider(minimum=0, maximum=32, step=1, label='Blending strides', value=1)
+            reuse_seed = gr.Checkbox(label='Reuse Seed', value=True)
+        
+        with gr.Row() as settings_row:
+            one_grid = gr.Checkbox(label='One grid', value=True)
+            interpolate_varseed = gr.Checkbox(label='Interpolate VarSeed', value=False)
+            paste_on_mask = gr.Checkbox(label='Paste on mask', value=False)
+            
+        loopback_toggle.change(change_visibility, show_progress=False, inputs=[loopback_toggle], outputs=loopback_vis) 
+
+            
+        return [init_img2, i_values, loopback_alpha, border_alpha, loopback_loops, blend_strides, loopback_toggle, reuse_seed, one_grid, interpolate_varseed, paste_on_mask]
+
+    def run(self, p, init_img2, i_values, loopback_alpha, border_alpha, loopback_loops, blend_strides, loopback_toggle, reuse_seed, one_grid, interpolate_varseed, paste_on_mask):
+        processing.fix_seed(p)
+        init_seed = p.seed
+        tick_seed = init_seed + 1
+        batch_count = p.n_iter
+        
+        p.extra_generation_params = {}
+
+        p.batch_size = 1
+        batch_count = 1
+        n = 0
+        p.n_iter = 1
+
+        output_images, info = None, "info test"
+        initial_seed = p.seed
+        #initial_info = create_infotext(p, p.prompt, [p.seed], [p.subseed], [])
+        var_seed_strength = p.subseed_strength
+
+        grids = []
+        all_images = []
+        all_images_grid = []
+
+        initial_color_corrections = [processing.setup_color_correction(p.init_images[0])]
+        
+        base_prompt = p.prompt
+        base_negative_prompt = p.negative_prompt
+        
+        init_img = p.init_images[0]
+        init_mask = None
+        
+        paste_full_res = True
+        
+        #cropping not supported?
+        crop_region = None
+        if p.image_mask is None:
+            init_img = images.resize_image(p.resize_mode, p.init_images[0], p.width, p.height)
+            if init_img2:
+                init_img2 = images.resize_image(p.resize_mode, init_img2, p.width, p.height)
+        else: 
+            init_mask = p.image_mask.convert('L')
+
+            if p.inpainting_mask_invert:
+                init_mask = ImageOps.invert(init_mask)
+
+            init_unblurred_mask = init_mask
+
+            if p.mask_blur > 0:
+                init_mask = init_mask.filter(ImageFilter.GaussianBlur(p.mask_blur))
+            
+            if p.inpaint_full_res:
+                #init_img = p.init_images[0].crop(crop_region)
+                #init_img = images.resize_image(2, init_img, p.width, p.height)
+                init_img = p.init_images[0]
+                if init_img2:
+                    if paste_on_mask:
+                        init_img_copy = init_img.copy()
+                        x1,y1,x2,y2 = masking.get_crop_region(np.array(init_mask),0)
+                        init_img_copy.paste(images.resize_image(p.resize_mode, init_img2, x2-x1, y2-y1), (x1,y1))
+                        init_img2 = init_img_copy
+                    else:
+                        w, h = init_img.size
+                        init_img2 = images.resize_image(p.resize_mode, init_img2, w, h)
+            else:
+                init_img = images.resize_image(p.resize_mode, p.init_images[0], p.width, p.height)
+                init_mask = images.resize_image(p.resize_mode, init_mask, p.width, p.height)
+                if init_img2:
+                    if paste_on_mask:
+                        init_img_copy = init_img.copy()
+                        x1,y1,x2,y2 = masking.get_crop_region(np.array(init_mask),0)
+                        init_img_copy.paste(images.resize_image(p.resize_mode, init_img2, x2-x1, y2-y1), (x1,y1))
+                        init_img2 = init_img_copy
+                    else:
+                        init_img2 = images.resize_image(p.resize_mode, init_img2, p.width, p.height)
+                
+                p.image_mask = init_mask
+        if not init_img2:
+            init_img2 = init_img
+        
+        history = []
+        
+        x = process_int(i_values)
+        
+        state.job_count = len(x) * batch_count * ( loopback_loops + 1 if loopback_toggle else 1 )
+    
+
+        
+        def process_list(img_in):
+            res = []
+            
+            for i in range(len(x)):
+            
+                pc = copy(p)
+                apply_prompt_interpolate(pc, x[i])
+                
+                if interpolate_varseed:
+                    pc.subseed_strength = var_seed_strength*x[i]
+            
+                #pc.init_images = [Image.blend(init_img, init_img2, min(1,max(0,x[i])))]
+                pc.init_images = [img_in[i]]
+                pc.n_iter = 1
+                pc.batch_size = 1
+                pc.do_not_save_grid = True
+
+                if opts.img2img_color_correction:
+                    pc.color_corrections = initial_color_corrections
+
+                state.job = f"Iteration {i + 1}/{len(x)}, batch {n + 1}/{batch_count}"
+
+                processed = processing.process_images(pc)
+                
+                if init_mask:
+                    res.append( Image.composite(processed.images[0], init_img, init_mask) )
+                else:
+                    res.append(processed.images[0])
+            
+            if not one_grid:
+                grid = images.image_grid(res, rows=1)
+                if opts.grid_save:
+                    images.save_image(grid, p.outpath_grids, "grid", initial_seed, p.prompt, opts.grid_format, info=info, short_filename=not opts.grid_extended_filename, grid=True, p=p)
+
+                grids.append(grid)
+            
+            return res
+            
+        def blend_images(base, pre, alpha, border_alpha, strides):
+            res = []
+            for i in range(len(base)):
+                if i == 0 or i == len(base)-1 or strides == 0:
+                    res.append(Image.blend(base[i], pre[i], border_alpha))
+                elif 0:
+                    temp = Image.blend(pre[i-1], pre[i+1], 0.5)
+                    blend = Image.blend(pre[i], temp, 0.5)
+                    res.append(Image.blend(base[i], blend, alpha))
+                elif 1:
+                    l = min(min(i,strides),min(len(base)-i-1,strides))
+                    #indices = list(set(range(i-l,i+l+1)) & set(range(len(base))) - set([i]))
+                    indices = list(set(range(i-l,i+l+1)) & set(range(len(base))))
+                    temp = np.asarray(pre[i])
+                    blend = np.zeros_like(temp)
+                    a = 1/len(indices)
+                    for j in indices:
+                        blend = blend + np.asarray(pre[j])*a
+                    #blend = temp*0.5 + blend*0.5
+                    res.append(Image.blend(base[i], Image.fromarray(blend.astype(np.uint8)), alpha))    
+                else:   #get 0.5 pre[i] plus an even mixture of all other images in range, has rounding issues
+                    indices = list(set(range(i-strides,i+strides+1)) & set(range(len(base))) - set([i]))
+                    temp = pre[i]
+                    a = 0.5/len(indices)
+                    for n,j in enumerate(indices):
+                        temp = Image.blend(temp, pre[j], a * (1-a)**(n+1-len(indices)))
+                    res.append(Image.blend(base[i], temp, alpha))    
+            return res
+                  
+            
+        #TODO: respect mask
+        if init_mask:
+            level0 = [Image.composite(Image.blend(init_img, init_img2, min(1,max(0,i))), init_img, init_mask) for i in x]
+        else:
+            level0 = [Image.blend(init_img, init_img2, min(1,max(0,i))) for i in x]
+        
+        level1 = process_list(level0)
+        all_images_grid += level1
+        all_images += level1
+ 
+        if loopback_toggle:
+            cur_level = level1
+            for i in range(loopback_loops):
+                if not reuse_seed:
+                    p.seed = p.seed + 1
+                    p.subseed = p.subseed + 1
+                cur_level = process_list( blend_images(level0, cur_level, loopback_alpha, border_alpha, blend_strides) )
+                all_images += cur_level
+                all_images_grid += cur_level
+            
+
+        if one_grid:
+            grid = images.image_grid(all_images_grid, rows=batch_count*(loopback_loops+1))
+            if opts.grid_save:
+                images.save_image(grid, p.outpath_grids, "grid", initial_seed, p.prompt, opts.grid_format, info=info, short_filename=not opts.grid_extended_filename, grid=True, p=p)
+
+                grids.append(grid)
+                
+        if opts.return_grid:
+            all_images = grids + all_images
+            
+        processed = Processed(p, all_images, initial_seed)
+
+        return processed
